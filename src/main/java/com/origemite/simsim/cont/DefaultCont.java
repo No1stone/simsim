cc356d0c546c56dd6d35ca2a7198f80e0b8784f3
jangwonseok <jangws1003@naver.com>
2024-03-06 17:08:08 +0900

    prefix sum

--- src/main/java/com/origemite/simsim/cont/DefaultCont.java
+++ src/main/java/com/origemite/simsim/cont/DefaultCont.java
@@ -1,5 +1,6 @@
 package com.origemite.simsim.cont;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.origemite.simsim.service.DefaultService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,9 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class DefaultCont {
     private final DefaultService defaultService;
+
     @GetMapping("/1")
-    public void defa1(){
-    defaultService.defa1();
+    public void defa1() throws JsonProcessingException {
+        int[] input = {1, 3, 11, 6, 7, 10, 14, 9, 18, 16, 5, 4, 2, 8, 19};
+        defaultService.defa1(input);
     }
 
 }
--- src/main/java/com/origemite/simsim/service/DefaultService.java
+++ src/main/java/com/origemite/simsim/service/DefaultService.java
@@ -1,5 +1,7 @@
 package com.origemite.simsim.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,8 +12,22 @@ import org.springframework.stereotype.Service;
 public class DefaultService {
 
     //prefixSum
-    public void defa1() {
+    public void defa1(int[] input) throws JsonProcessingException {
+        int n = input.length + 1;
+        log.info("n = {}", n);
+        int[] preSum = new int[n];
+        log.info("{}", new ObjectMapper().writeValueAsString(preSum));
 
+        for (int i = 1; i < n; i++) {
+            log.info("i = {}", i);
+            preSum[i] = preSum[i - 1] + input[i - 1];
+//            log.info("{}", new ObjectMapper().writeValueAsString(preSum));
+            log.info("{}", input[i - 1]);
+        }
+        log.info("{}", new ObjectMapper().writeValueAsString(preSum));
+        log.info("{}", new ObjectMapper().writeValueAsString(preSum[4]));
+        log.info("{}", new ObjectMapper().writeValueAsString(preSum[2]));
+        log.info("{}", new ObjectMapper().writeValueAsString(preSum[4] - preSum[2 - 1]));
 
     }
 }
